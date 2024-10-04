f806769b3388257b9c38c68ab36cf0ca9b668c60
Kaustubha Venkata Eluri <venkatakaustubha@gmail.com>
2024-10-04 11:30:05 -0700

    a2

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,47 +1,46 @@
+import React from 'react';
 import { MdDoNotDisturbAlt } from 'react-icons/md';
 import { FaCheckCircle } from 'react-icons/fa';
 import { BiImport } from 'react-icons/bi';
 import { LiaFileImportSolid } from 'react-icons/lia';
-{
-  /* Find more icons */
-}
+
 export default function CourseStatus() {
   return (
     <div id="wd-course-status" style={{ width: '300px' }}>
       <h2>Course Status</h2>
       <div className="d-flex">
         <div className="w-50 pe-1">
-          <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
-            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish{' '}
+          <button className="btn btn-lg btn-secondary w-100 text-nowrap">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish
           </button>
         </div>
         <div className="w-50">
           <button className="btn btn-lg btn-success w-100">
-            <FaCheckCircle className="me-2 fs-5" /> Publish{' '}
+            <FaCheckCircle className="me-2 fs-5" /> Publish
           </button>
         </div>
       </div>
       <br />
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <BiImport className="me-2 fs-5" /> Import Existing Content{' '}
+        <BiImport className="me-2 fs-5" /> Import Existing Content
       </button>
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons{' '}
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons
       </button>
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" /> Choose From Homepage{' '}
+        <LiaFileImportSolid className="me-2 fs-5" /> Choose From Homepage
       </button>
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" /> View Course Stream{' '}
+        <LiaFileImportSolid className="me-2 fs-5" /> View Course Stream
       </button>
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" /> New Announcement{' '}
+        <LiaFileImportSolid className="me-2 fs-5" /> New Announcement
       </button>
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" /> New Analytics{' '}
+        <LiaFileImportSolid className="me-2 fs-5" /> New Analytics
       </button>
       <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
-        <LiaFileImportSolid className="me-2 fs-5" /> View Course Notifications{' '}
+        <LiaFileImportSolid className="me-2 fs-5" /> View Course Notifications
       </button>
     </div>
   );
