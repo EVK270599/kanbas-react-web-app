6dbdddeab1193fca77f6c68f58baa1e2a55303af
Kaustubha Venkata Eluri <venkatakaustubha@gmail.com>
2024-10-03 23:01:57 -0700

    Your commit message for branch a1

---
+++ src/Kanbas/Courses/index.tsx
@@ -0,0 +1,31 @@
+import { Navigate, Route, Routes } from 'react-router';
+import CoursesNavigation from './Navigation';
+import Modules from './Modules';
+import Home from './Home';
+import Assignments from './Assignments';
+import AssignmentEditor from './Assignments/Editor';
+
+export default function Courses() {
+  return (
+    <div id="wd-courses">
+      <h2>Course 1234</h2>
+      <table>
+        <tr>
+          <td valign="top">
+            <CoursesNavigation />
+          </td>
+          <td>
+            <Routes>
+              <Route path="/" element={<Navigate to="Home" />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="Modules" element={<Modules />} />
+              <Route path="Assignments" element={<Assignments />} />
+              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+              <Route path="People" element={<h3>People</h3>} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
---
+++ src/Labs/Lab3/index.tsx
@@ -0,0 +1,7 @@
+export default function Labs2() {
+    return (
+      <div>
+        <h2>Lab 3</h2>
+      </div>
+    );
+  }
