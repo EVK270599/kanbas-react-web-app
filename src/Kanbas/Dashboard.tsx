1535551a029fb2f1b6a68d54a0480f2d89275abe
Kaustubha Venkata Eluri <venkatakaustubha@gmail.com>
2024-10-03 23:15:34 -0700

    a1 commits

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
@@ -12,11 +12,11 @@ export default function Dashboard() {
               className="wd-dashboard-course-link"
               to="/Kanbas/Courses/1234/Home"
             >
-              {' '}
-              CS1234 React JS{' '}
+              {" "}
+              CS1234 React JS{" "}
             </Link>
             <p className="wd-dashboard-course-title">
-              Full Stack software developer{' '}
+              Full Stack software developer{" "}
             </p>
             <Link to="/Kanbas/Courses/1234/Home"> Go </Link>
           </div>
@@ -28,11 +28,11 @@ export default function Dashboard() {
               className="wd-dashboard-course-link"
               to="/Kanbas/Courses/1234/Home"
             >
-              {' '}
-              CS1234 React JS{' '}
+              {" "}
+              CS1234 React JS{" "}
             </Link>
             <p className="wd-dashboard-course-title">
-              Full Stack software developer{' '}
+              Full Stack software developer{" "}
             </p>
             <Link to="/Kanbas/Courses/1234/Home"> Go </Link>
           </div>
@@ -44,15 +44,31 @@ export default function Dashboard() {
               className="wd-dashboard-course-link"
               to="/Kanbas/Courses/1234/Home"
             >
-              {' '}
-              CS1234 React JS{' '}
+              {" "}
+              CS1234 React JS{" "}
             </Link>
             <p className="wd-dashboard-course-title">
-              Full Stack software developer{' '}
+              Full Stack software developer{" "}
             </p>
             <Link to="/Kanbas/Courses/1234/Home"> Go </Link>
           </div>
         </div>
+        <div className="wd-dashboard-course">
+          <img src="/images/reactjs.jpg" width={200} />
+          <div>
+            <Link
+              className="wd-dashboard-course-link"
+              to="/Kanbas/Courses/1234/Home"
+            >
+              {" "}
+              CS1234 React JS{" "}
+            </Link>
+            <p className="wd-dashboard-course-title">
+              Full Stack software developer{" "}
+            </p>
+            <Link to="/Kanbas/Courses/1234/Home"> Go </Link>
+          </div>
+        </div>{" "}
       </div>
     </div>
   );
